74302adb8d48dadd2cdf133fc624c621dca12484
vagiz <ahatovvagiz@gmail.com>
2023-12-28 21:25:16 +0300

    seminar1

--- Program.cs
+++ Program.cs
@@ -151,35 +151,119 @@
 //     }
 // }
 
+// using System;
+// using System.ComponentModel.DataAnnotations;
+
+// public class Answer {
+    
+//   static int FindMax(int a, int b, int c)
+//     {
+//       // Введите свое решение ниже
+//         int max = a;
+//         if (b>max)
+//         {
+//             max = b;
+//         }
+//         if (c>max)
+//         {
+//             max = c;
+//         }
+//         return max;
+//     }
+
+//   // Не удаляйте и не меняйте метод Main! 
+//   static public void Main(string[] args) 
+//     {
+//         int a, b, c;
+
+//         if (args.Length >= 3) {
+//            a = int.Parse(args[0]);
+//            b = int.Parse(args[1]);
+//            c = int.Parse(args[2]);
+//         } else {
+//            // Здесь вы можете поменять значения для отправки кода на Выполнение
+//            a = 8;
+//            b = 6;
+//            c = 7;
+//         }
+
+//         // Не удаляйте строки ниже
+//         int result = FindMax(a, b, c);
+//         System.Console.WriteLine($"{result}");
+//     }
+// }
+
+// using System;
+
+// class Answer {
+//     static void CheckIfEven(int number)
+//     {
+//       // Введите свое решение ниже
+//         int rem = number % 2;
+//         if (rem == 0)
+//         {
+//             System.Console.WriteLine($"Число `{number}` чётное");
+//         }
+//         else
+//         {
+//             System.Console.WriteLine($"Число `{number}` нечётное");    
+//         }          
+//     }
+  
+
+//   // Не удаляйте и не меняйте метод Main! 
+//     static public void Main(string[] args) {
+//         int number;
+
+//         if (args.Length >= 1) {
+//             number = int.Parse(args[0]);
+//         } else {
+//            // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             number = 6;
+//         }
+
+//         // Не удаляйте строки ниже
+//         CheckIfEven(number);
+//     }
+// }
+
+// number = 5 -> 2    4
+
+// number = 8 -> 2    4    6    8
 using System;
 
 public class Answer {
-    
-  static int FindMax(int a, int b, int c)
+    static void PrintEvenNumbers(int number)
     {
       // Введите свое решение ниже
+        int n = 1;
+        int rem;
 
-
-
+        while (n<=number)
+        {
+            rem = n % 2;
+            if (rem == 0)
+            {
+                System.Console.Write(n+"\t");        
+            }
+            n++;
+        }
+        
     }
 
-  // Не удаляйте и не меняйте метод Main! 
-  static public void Main(string[] args) {
-        int a, b, c;
 
-        if (args.Length >= 3) {
-           a = int.Parse(args[0]);
-           b = int.Parse(args[1]);
-           c = int.Parse(args[2]);
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
         } else {
            // Здесь вы можете поменять значения для отправки кода на Выполнение
-           a = 5;
-           b = 6;
-           c = 7;
+            number = 5;
         }
 
         // Не удаляйте строки ниже
-        int result = FindMax(a, b, c);
-        System.Console.WriteLine($"{result}");
+        PrintEvenNumbers(number);
     }
 }
