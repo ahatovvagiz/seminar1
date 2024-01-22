87f4433e77570fb6703a21972aca05f4bdc8110f
vagiz <ahatovvagiz@gmail.com>
2024-01-22 14:01:07 +0300

    добавили task

--- Program.cs
+++ Program.cs
@@ -230,40 +230,40 @@
 // number = 5 -> 2    4
 
 // number = 8 -> 2    4    6    8
-using System;
+// using System;
 
-public class Answer {
-    static void PrintEvenNumbers(int number)
-    {
-      // Введите свое решение ниже
-        int n = 1;
-        int rem;
+// public class Answer {
+//     static void PrintEvenNumbers(int number)
+//     {
+//       // Введите свое решение ниже
+//         int n = 1;
+//         int rem;
 
-        while (n<=number)
-        {
-            rem = n % 2;
-            if (rem == 0)
-            {
-                System.Console.Write(n+"\t");        
-            }
-            n++;
-        }
+//         while (n<=number)
+//         {
+//             rem = n % 2;
+//             if (rem == 0)
+//             {
+//                 System.Console.Write(n+"\t");        
+//             }
+//             n++;
+//         }
         
-    }
+//     }
 
 
-  // Не удаляйте и не меняйте метод Main! 
-    static public void Main(string[] args) {
-        int number;
+//   // Не удаляйте и не меняйте метод Main! 
+//     static public void Main(string[] args) {
+//         int number;
 
-        if (args.Length >= 1) {
-            number = int.Parse(args[0]);
-        } else {
-           // Здесь вы можете поменять значения для отправки кода на Выполнение
-            number = 5;
-        }
+//         if (args.Length >= 1) {
+//             number = int.Parse(args[0]);
+//         } else {
+//            // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             number = 5;
+//         }
 
-        // Не удаляйте строки ниже
-        PrintEvenNumbers(number);
-    }
-}
+//         // Не удаляйте строки ниже
+//         PrintEvenNumbers(number);
+//     }
+// }
